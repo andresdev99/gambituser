302c50400588c7d41b145a3334e0fafedc68bfe5
Andres Gutierrez <andresfelix951@gmail.com>
2025-04-01 00:58:08 -0500

    subo

--- subo.bat
+++ subo.bat
@@ -5,5 +5,4 @@ set GOOS=windows
 set GOARCH=amd64
 go build -tags lambda.norpc -o bootstrap main.go
 del main.zip
-chcp 65001
-Compress-Archive -Path main -DestinationPath main.zip -Force
+tar.exe -a -cf main.zip main
