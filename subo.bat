cd45b29a9d4ee18f7c9680527121ecba3d5f5b35
Andres Gutierrez <andresfelix951@gmail.com>
2025-04-01 10:41:22 -0500

    subo

--- subo.bat
+++ subo.bat
@@ -1,9 +1,8 @@
 git add .
 git commit -m "subo"
 git push
-set GOOS=linux
+set GOOS=windows
 set GOARCH=amd64
-@REM go build -tags lambda.norpc -o bootstrap main.go
-go build main.go
+go build -tags lambda.norpc -o bootstrap main.go
 del main.zip
 tar.exe -a -cf main.zip main
